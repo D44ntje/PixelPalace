06675e6da055c1b0996ed1d31aebf061a92496b7
Daan <daan.vandermeulen@student.hu.nl>
2024-10-02 09:08:26 +0200

    feature

--- nano/Overige/Dobbelen.py
+++ nano/Overige/Dobbelen.py
@@ -12,9 +12,20 @@ def start_up():
         aantal = int(input("Hoeveel dobbelstenen wil je gooien? "))
         if aantal > 0:
             worpen = gooi_dobbelstenen(aantal)
-            print(f"Je hebt {aantal} dobbelstenen gegooid. De resultaten zijn: {worpen}")
+            print(f"\nJe hebt {aantal} dobbelstenen gegooid. De resultaten zijn: {worpen}")
             print(f"Het totaal van die dobbelstenen is: {sum(worpen)}")
         else:
             print("Het aantal dobbelstenen moet positief zijn.")
     except ValueError:
         print("Voer alstublieft een numerieke waarde in.")
+        start_up()
+    def play_again():
+        playagain = input("\nWil je nog een keer dobbelen?\n(Voer 'ja' of 'nee' in: ")
+        if playagain.lower() == "ja":
+            start_up()
+        elif playagain.lower() == "nee":
+            print("\nDankjewel voor het dobbelen!")
+        else:
+            play_again()
+    play_again()
+start_up()
